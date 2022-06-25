cd16a8b780cde78a8e4baa4947d33ea9eda94132
teagueStockwell <tsAppDevelopment@gmail.com>
2022-06-25 10:57:19 -0700

    Add missing dev deps

--- src/create.test.ts
+++ src/create.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import { create } from './create';
 
 jest.mock('lodash.debounce', () => ({
